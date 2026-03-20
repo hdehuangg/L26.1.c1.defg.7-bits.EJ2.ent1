866bd0b721959ee9a443ca65b4a6addf3b2bf73a
hdehuangg <diazhuangjhonnraikar@gmail.com>
2026-03-19 22:53:30 -0400

    Delete L26.1.c1.defg.7-bits.EJ2.ent1 directory

--- L26.1.c1.defg.7-bits.EJ2.ent1/src/interfaces/I_vEquipo.ts
+++
@@ -1,4 +0,0 @@
-export interface I_vEquipo {
-  onNuevoArticulo(callback: () => void): void;
-  reportar(resultados: number): void;
-}
--- L26.1.c1.defg.7-bits.EJ2.ent1/src/interfaces/I_vJuego.ts
+++
@@ -1,8 +0,0 @@
-export interface I_vJuego {
-  get resultado(): number;
-  mostrar(): void;
-  ocultar(): void;
-  onAgregar(callback: () => void): void;
-  onAceptar(callback: () => void): void;
-  onCancelar(callback: () => void): void;
-}
